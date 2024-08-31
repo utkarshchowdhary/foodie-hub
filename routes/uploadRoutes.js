f289f8ca72c8919542195511376737a7b73553be
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2024-08-31 22:47:30 +0530

    chore: allow only chefs to upload images

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+const { verifyToken, isChef } = require('../middlewares/authMiddleware');
 const upload = require('../middlewares/uploadMiddleware');
 const { uploadImage } = require('../controllers/uploadController');
 
 const router = express.Router();
 
 // Route for uploading recipe images (cover_image or content_image)
-router.post('/', upload.single('image'), uploadImage);
+router.post('/', verifyToken, isChef, upload.single('image'), uploadImage);
 
 module.exports = router;
