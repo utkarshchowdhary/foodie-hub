66c0d58ad936d327ed8c33974ba90449d57d9edd
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2024-09-01 09:57:02 +0530

    feat: WIP: create-recipe API, handle recipe content creation

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,7 +1,40 @@
 const db = require('../config/db');
 
 const createRecipe = async (req, res) => {
-    res.status(201).json({ status: 'success' });
+    const { id: chefId } = req.userData;
+    const { title, cover_image, draft, prep_time, cook_time, servings, ingredients, content } =
+        req.body;
+
+    try {
+        // Insert the recipe into the recipes table
+        const [{ insertId: recipeId }] = await db.execute(
+            'INSERT INTO recipes (chef_id, title, cover_image, draft, prep_time, cook_time, servings) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [chefId, title, cover_image, draft, prep_time, cook_time, servings]
+        );
+
+        // Insert content into the recipe_content table
+        for (let i = 0; i < content.length; i++) {
+            const widget = content[i];
+
+            await db.execute(
+                'INSERT INTO recipe_content (recipe_id, content_type, content_heading, content_text, content_image, display_order) VALUES (?, ?, ?, ?, ?, ?)',
+                [
+                    recipeId,
+                    widget.content_type,
+                    widget.content_heading,
+                    widget.content_text,
+                    widget.content_image,
+                    i // Using the index for display order
+                ]
+            );
+        }
+
+        res.status(201).json({ status: 'success' });
+    } catch (error) {
+        console.error('Error creating recipe.', error);
+
+        res.status(500).json({ status: 'error', message: 'Internal server error' });
+    }
 };
 
 const getRecipes = async (req, res) => {
@@ -10,6 +43,8 @@ const getRecipes = async (req, res) => {
 
         res.status(200).json({ status: 'success', data: recipes });
     } catch (error) {
+        console.error('Error getting recipes.', error);
+
         res.status(500).json({ status: 'error', message: 'Internal server error' });
     }
 };
