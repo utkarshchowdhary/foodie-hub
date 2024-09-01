faef1edb350ab7b9dc016c7f1b8d26731b3a4867
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2024-09-01 09:46:59 +0530

    chore: set zod data after validation to request body

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -1,11 +1,12 @@
 // Middleware function to validate request body
 const validateRequest = schema => (req, res, next) => {
-    const { success, error } = schema.safeParse(req.body);
+    const { success, data, error } = schema.safeParse(req.body);
 
     if (!success) {
         return res.status(400).json({ status: 'error', error: error.errors });
     }
 
+    req.body = data;
     next();
 };
 
